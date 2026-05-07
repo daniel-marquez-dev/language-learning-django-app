252ac7b49c51deeb0e0d2c4409960f81ba77303b
Daniel Marquez Quintero <daniel.marquez@hahcompany.com>
2026-05-07 13:49:30 +0200

    Modify the name of the path route

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.word_list, name='word_list'),
-    path('add/', views.add_word, name='add_word'),
+    path('add-word/', views.add_word, name='add_word'),
 ]

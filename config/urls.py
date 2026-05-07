6d82c8eb36c80b06e5e986d10bb4a5ab6b33406e
DanielMelgar <daniel.melgar@hahcompany.com>
2026-05-07 15:27:31 +0200

    quiz urls, html and code

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vocabulary/', include('vocabulary.urls')),
+    path('vocabulary/', include('vocabulary.urls')), # Rutas del Intern 1
+    path('quiz/', include('quiz.urls')),             # TUS rutas de Intern 2
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,27 @@
+import random
 from django.shortcuts import render
+from vocabulary.models import Word  # Importas el modelo desde la otra app
 
-# Create your views here.
+def quiz_view(request):
+    result = None
+    selected_word = None
+    
+    if request.method == "POST":
+        word_id = request.POST.get("word_id")
+        user_answer = request.POST.get("answer").lower().strip()
+        correct_word = Word.objects.get(id=word_id)
+        
+        if user_answer == correct_word.translation.lower().strip():
+            result = "✅ ¡Correcto!"
+        else:
+            result = f"❌ Incorrecto. Era: {correct_word.translation}"
+        selected_word = correct_word 
+    else:
+        words = Word.objects.all()
+        if words.exists():
+            selected_word = random.choice(words)
+
+    return render(request, "quiz/quiz_page.html", {
+        "word": selected_word,
+        "result": result
+    })

155c9a6ef91e8a49d3a668382d5354ac993aa2f5
Daniel Marquez Quintero <daniel.marquez@hahcompany.com>
2026-05-11 10:55:38 +0200

    Modified the urls.py, change the name path of the wordlist

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.word_list, name='word_list'),
+    path('words/', views.word_list, name='word_list'),
     path('add-word/', views.add_word, name='add_word'),
 ]

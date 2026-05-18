539662d191a1132b9cc4db9b434ba7d2d421cc6e
Daniel Marquez Quintero <daniel.marquez@hahcompany.com>
2026-05-18 15:56:43 +0200

    Added the select languages on 'add word'

--- vocabulary/forms.py
+++ vocabulary/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Word
+from django.core.validators import RegexValidator
 
 class WordForm(forms.ModelForm):
-   class Meta:
-       model = Word
-       fields = ["original", "translation", "language"]
+    # 1. Definimos el validador
+    word_validate = RegexValidator(
+        regex=r'^[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ\s-]+$',
+        message="The word cannot contain special characters like @, _, /, or numbers. Only letters and spaces are allowed."
+    )
+
+    original = forms.CharField(validators=[word_validate])
+    translation = forms.CharField(validators=[word_validate])
+
+    class Meta:
+        model = Word
+        fields = ["original", "translation", "language"]
--- vocabulary/models.py
+++ vocabulary/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 
 class Word(models.Model):
-   original = models.CharField(max_length=100)
-   translation = models.CharField(max_length=100)
-   language = models.CharField(max_length=50)
- 
-   def __str__(self):
-       return f"{self.original} - {self.translation}"
+
+    LANGUAGE_CHOICES = [
+        ('sp', 'Spanish'),
+        ('en', 'English'),
+        ('pl', 'Polski'),
+    ]
+
+    original = models.CharField(max_length=100)
+    translation = models.CharField(max_length=100)
+    
+
+    language = models.CharField(
+        max_length=2,
+        choices=LANGUAGE_CHOICES,
+        default='es',
+    )
+
+    def __str__(self):
+        return f"{self.original} -> {self.translation} ({self.get_language_display()})"
